11bacc6da9f41532531117dc62e348f57e02a1ab
cla <me@mail.com>
2024-04-17 11:08:46 -0300

    add challenge of day 10

---
+++ 10-binary-numbers.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    binary_number = []
+
+    # Convert input to binary number
+    while n > 0:
+        remainder = n % 2
+        binary_number.append(remainder)
+
+        # Update n with int result number
+        n = n // 2
+
+    consecutive_ones = 0
+    max_number_consecutive_ones = 0
+
+    # Check consecutive ones
+    for i in range(len(binary_number)):
+        # Check the begining of consecutive ones
+        if binary_number[i] == 1:
+            consecutive_ones += 1
+            if consecutive_ones > max_number_consecutive_ones:
+                max_number_consecutive_ones = consecutive_ones
+        # Restart count
+        else:
+            consecutive_ones = 0
+
+    print(max_number_consecutive_ones)
--- 11-2D-arrays.py
+++ 11-2D-arrays.py
@@ -14,16 +14,21 @@ if __name__ == '__main__':
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
 
-    max_sum_of_hourglasses = float('-inf')
+    # Initialize max sum of hourglasses to negative infinity
+    max_sum_of_hourglass = float('-inf')
 
     for i in range(6):
+        # Pattern of hourglass 3 - 2 - 3
         if i <= 3:
             for j in range(6):
                 if j <= 3:
+
+                    # Sum values of hourglass
                     sum_of_hourglass = sum(
                         arr[i][j:j+3]) + arr[i + 1][j + 1] + sum(arr[i + 2][j:j+3])
 
-                    if sum_of_hourglass > max_sum_of_hourglasses:
-                        max_sum_of_hourglasses = sum_of_hourglass
+                    # Update max sum of hourglass
+                    if sum_of_hourglass > max_sum_of_hourglass:
+                        max_sum_of_hourglass = sum_of_hourglass
 
-    print(max_sum_of_hourglasses)
+    print(max_sum_of_hourglass)
